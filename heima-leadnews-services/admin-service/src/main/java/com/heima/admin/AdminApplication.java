1a70bd0c6847481cd8cb6b54dd94d8e1d6148c38
NJY <850738958@qq.com>
2023-08-17 14:45:43 +0800

    集成nacos

--- heima-leadnews-services/admin-service/src/main/java/com/heima/admin/AdminApplication.java
+++ heima-leadnews-services/admin-service/src/main/java/com/heima/admin/AdminApplication.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -16,6 +17,7 @@ import org.springframework.context.annotation.Bean;
  * @say 山河总静好，人事也从容
  */
 @SpringBootApplication(scanBasePackages = {"com.heima"})
+@EnableDiscoveryClient
 @MapperScan("com.heima.admin.mapper")
 public class AdminApplication {
     public static void main(String[] args) {
