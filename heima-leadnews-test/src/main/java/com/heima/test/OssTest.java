425544720a52131b7e32a02dcdd20d40e62bbc0f
NJY <850738958@qq.com>
2023-08-29 16:23:51 +0800

    隐藏osstoken

--- heima-leadnews-test/src/main/java/com/heima/test/OssTest.java
+++ heima-leadnews-test/src/main/java/com/heima/test/OssTest.java
@@ -18,10 +18,14 @@ import java.io.FileInputStream;
 public class OssTest {
     public static void main(String[] args) throws Exception{
         // Endpoint以杭州为例，其它Region请按实际情况填写。
-        String endpoint = "http://oss-cn-shanghai.aliyuncs.com";
+        String endpoint = "";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。
-        String accessKeyId = "LTAI4G8RTSUp2UuN1Ab7AhrZ";
-        String accessKeySecret = "d4idycRKzMjrQOib60ZMVTTkBWeAUO";
+        String accessKeyId = "";
+        String accessKeySecret = "";
+//        String endpoint = "http://oss-cn-shanghai.aliyuncs.com";
+//        // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。
+//        String accessKeyId = "LTAI4G8RTSUp2UuN1Ab7AhrZ";
+//        String accessKeySecret = "d4idycRKzMjrQOib60ZMVTTkBWeAUO";
 
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
